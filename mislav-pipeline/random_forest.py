efaaaf12a9aa40b027624407bc88db855133918c
Garrett Smith <g@rre.tt>
2020-07-29 12:23:48 -0500

    Update with second pipeline example showing flag pass through

--- mislav-pipeline/random_forest.py
+++ mislav-pipeline/random_forest.py
@@ -1,6 +1,11 @@
 import util
 
+depth = 1
+
 data, meta = util.load_data()
 
-print("Training random forest model on %s (%i examples)" % (meta["type"], len(data)))
+print(
+    "Training random forest model on %s (%i examples) with depth %i"
+    % (meta["type"], len(data), depth)
+)
 print("acc: 0.9")
